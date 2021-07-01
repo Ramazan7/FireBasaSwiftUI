b629387a64214502f126153fd6cc71713b1ef0e1
Ramazan <ramazan05.00@mail.ru>
2021-07-01 16:49:40 +0300

    Initial Commit

---
+++ FireBaseSwiftUI/FireBaseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseSwiftUIApp.swift
+//  FireBaseSwiftUI
+//
+//  Created by Admin on 01.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
